428d9aaa1067d397f1da02228bb61faf2b35c5c6
ImperatorMarsa <tima.sitnikov@mail.ru>
2023-10-23 09:56:11 +0300

    Добавил перемешение установленных плагинов в глобальную досутпность

--- src/instal-gnome-shell-extensions.sh
+++ src/instal-gnome-shell-extensions.sh
@@ -3,18 +3,23 @@
 mkdir -p /var/roothome/.cache
 mkdir -p /tmp/gnome_extensions
 
-### Vitas
+### Vitas ###########################################################################################################################
 # Download
 mkdir -p /tmp/gnome_extensions/vitals
 cd /tmp/gnome_extensions/vitals
 wget https://extensions.gnome.org/extension-data/VitalsCoreCoding.com.v63.shell-extension.zip
 # Install
-gnome-extensions install /tmp/gnome_extensions/vitals/VitalsCoreCoding.com.v63.shell-extension.zip
+gnome-extensions install -f /tmp/gnome_extensions/vitals/VitalsCoreCoding.com.v63.shell-extension.zip
+#####################################################################################################################################
 
-### Forge
+### Forge ###########################################################################################################################
 # Download
 mkdir -p /tmp/gnome_extensions/forge
 cd /tmp/gnome_extensions/forge
 wget https://extensions.gnome.org/extension-data/forgejmmaranan.com.v75.shell-extension.zip
 # Install
-gnome-extensions install /tmp/gnome_extensions/forge/forgejmmaranan.com.v75.shell-extension.zip
+gnome-extensions install -f /tmp/gnome_extensions/forge/forgejmmaranan.com.v75.shell-extension.zip
+#####################################################################################################################################
+
+### Make extensions global available
+mv /root/.local/share/gnome-shell/extensions/* /usr/share/gnome-shell/extensions/
