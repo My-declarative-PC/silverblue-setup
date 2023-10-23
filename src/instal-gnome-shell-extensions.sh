a41d401d1e50eca02676fba376d04b87d15c59df
ImperatorMarsa <tima.sitnikov@mail.ru>
2023-10-23 20:23:50 +0300

    скоректировал копирование расширений

--- src/instal-gnome-shell-extensions.sh
+++ src/instal-gnome-shell-extensions.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+mkdir -p /usr/share/gnome-shell/extensions
 mkdir -p /var/roothome/.cache
 mkdir -p /tmp/gnome_extensions
 
@@ -10,6 +11,8 @@ cd /tmp/gnome_extensions/vitals
 wget https://extensions.gnome.org/extension-data/VitalsCoreCoding.com.v63.shell-extension.zip
 # Install
 gnome-extensions install -f /tmp/gnome_extensions/vitals/VitalsCoreCoding.com.v63.shell-extension.zip
+# Make extension global available
+/root/.local/share/gnome-shell/extensions/Vitals@CoreCoding.com /usr/share/gnome-shell/extensions/Vitals@CoreCoding.com
 #####################################################################################################################################
 
 ### Forge ###########################################################################################################################
@@ -19,7 +22,8 @@ cd /tmp/gnome_extensions/forge
 wget https://extensions.gnome.org/extension-data/forgejmmaranan.com.v75.shell-extension.zip
 # Install
 gnome-extensions install -f /tmp/gnome_extensions/forge/forgejmmaranan.com.v75.shell-extension.zip
+# Make extension global available
+/root/.local/share/gnome-shell/extensions/forge@jmmaranan.com /usr/share/gnome-shell/extensions/forge@jmmaranan.com
 #####################################################################################################################################
 
-### Make extensions global available
-mv /root/.local/share/gnome-shell/extensions/* /usr/share/gnome-shell/extensions/
+-
