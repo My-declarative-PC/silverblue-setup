4f25347f6229375351b7181000c407dde842b97e
ImperatorMarsa <tima.sitnikov@mail.ru>
2023-10-23 21:27:12 +0300

    дописла команду перемешения файла

--- src/instal-gnome-shell-extensions.sh
+++ src/instal-gnome-shell-extensions.sh
@@ -12,7 +12,7 @@ wget https://extensions.gnome.org/extension-data/VitalsCoreCoding.com.v63.shell-
 # Install
 gnome-extensions install -f /tmp/gnome_extensions/vitals/VitalsCoreCoding.com.v63.shell-extension.zip
 # Make extension global available
-/root/.local/share/gnome-shell/extensions/Vitals@CoreCoding.com /usr/share/gnome-shell/extensions
+mv /root/.local/share/gnome-shell/extensions/Vitals@CoreCoding.com /usr/share/gnome-shell/extensions
 #####################################################################################################################################
 
 ### Forge ###########################################################################################################################
@@ -23,6 +23,6 @@ wget https://extensions.gnome.org/extension-data/forgejmmaranan.com.v75.shell-ex
 # Install
 gnome-extensions install -f /tmp/gnome_extensions/forge/forgejmmaranan.com.v75.shell-extension.zip
 # Make extension global available
-/root/.local/share/gnome-shell/extensions/forge@jmmaranan.com /usr/share/gnome-shell/extensions
+mv /root/.local/share/gnome-shell/extensions/forge@jmmaranan.com /usr/share/gnome-shell/extensions
 #####################################################################################################################################
 
