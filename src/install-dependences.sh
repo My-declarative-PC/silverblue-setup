fb0b41d01e364bd1e8f8b736ddf3a5261b42a577
ImperatorMarsa <tima.sitnikov@mail.ru>
2023-10-25 14:18:35 +0300

    Добавил функцию которая перед тем как установить пакет проверяет его наличее

--- src/install-dependences.sh
+++ src/install-dependences.sh
@@ -1,14 +1,29 @@
 #! /bin/bash
 
+install_packages() {
+    local packages=("$@")
+    
+    for package in "${packages[@]}"; do
+        if ! rpm-ostree status "${package}" &> /dev/null; then
+            echo "Warning: Package '${package}' not found in repository."
+        else
+            rpm-ostree install "${package}"
+        fi
+    done
+}
+
 # Shell Extensions
-rpm-ostree install \
-    gnome-shell-extension-blur-my-shell \
-    gnome-shell-extension-caffeine \
-    gnome-shell-extension-gsconnect \
-    gnome-shell-extension-system-monitor-applet \
-    gnome-shell-extension-gpaste \
-    gnome-shell-extension-bubblemail \
-    gnome-shell-extension-forge
+packages=( \
+    "gnome-shell-extension-blur-my-shell" \
+    "gnome-shell-extension-caffeine" \
+    "gnome-shell-extension-gsconnect" \
+    "gnome-shell-extension-system-monitor-applet" \
+    "gnome-shell-extension-gpaste" \
+    "gnome-shell-extension-bubblemail" \
+    "gnome-shell-extension-forge" \
+    "gnome-shell-extension-forge2" \
+)
+install_packages "${packages[@]}"
 
 # Fonts
 rpm-ostree install \
