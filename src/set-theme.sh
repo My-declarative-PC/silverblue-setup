14e935f4e31d242c02d29d38903f952094fb5391
Timofey Sitnikov <t.sitnikov@infocom-s.ru>
2023-11-14 10:58:00 +0300

    Перенёс задание темы в отдельный конфиг темы

--- src/set-theme.sh
+++ src/set-theme.sh
@@ -3,41 +3,43 @@
 mkdir -p /tmp/gtk_theme
 THONE=Lavender
 
-# download theme
+### GTK
+# download
 wget https://github.com/catppuccin/gtk/releases/latest/download/Catppuccin-Mocha-Standard-${THONE}-Dark.zip -O /tmp/gtk_theme/gtk_theme.zip
 cd /tmp/gtk_theme
 unzip gtk_theme.zip
-# setup theme
+# setup
 mkdir -p /usr/share/themes
 mv Catppuccin-Mocha-Standard-${THONE}-Dark /usr/share/themes/Catppuccin
 gsettings set org.gnome.desktop.interface gtk-theme "Catppuccin"
 gsettings set org.gnome.desktop.wm.preferences theme "Catppuccin"
 
-# download icons
+### Icons
+# download
 git clone https://github.com/catppuccin/papirus-folders.git
 cd papirus-folders
-# setup icons
+# setup
 cp -r src/* /usr/share/icons/Papirus
 chmod +x ./papirus-folders
 ./papirus-folders -C cat-mocha-${THONE,,} --theme Papirus-Dark
 
-# download cursors
+### Cursors
+# download
 wget https://github.com/catppuccin/cursors/releases/latest/download/Catppuccin-Mocha-${THONE}-Cursors.zip -O /tmp/gtk_theme/cursors.zip
 cd /tmp/gtk_theme
 unzip cursors.zip
-# setup cursors
+# setup
 cp -r Catppuccin-Mocha-${THONE}-Cursors /usr/share/icons
 
-# download sddm-theme
-mkdir /tmp/theme_sddm
-cd    /tmp/theme_sddm
+### Display manager (SDDM)
+# download
+mkdir -p /tmp/theme_sddm
+cd /tmp/theme_sddm
 git clone https://github.com/catppuccin/sddm.git
-# set sddm-theme
+# setup
 mkdir -p /usr/share/sddm/themes
-# mkdir -p /etc/sddm.conf.d
+mkdir -p /etc/sddm.conf.d
 cp -r sddm/src/catppuccin-mocha /usr/share/sddm/themes/
-# touch /etc/sddm.conf.d/theme.conf
-# echo  '[Theme]
-# Current=catppuccin-mocha' > /etc/sddm.conf.d/theme.conf
-echo '[Theme]
-Current=catppuccin-mocha' >> /etc/sddm.conf
+touch /etc/sddm.conf.d/theme.conf
+echo  '[Theme]
+Current=catppuccin-mocha' > /etc/sddm.conf.d/theme.conf
