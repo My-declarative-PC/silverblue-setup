e1ddabcaee33bd7fdcad2ab88d94b79263904280
Timofey Sitnikov <t.sitnikov@infocom-s.ru>
2023-11-16 12:29:51 +0300

    Разбил скрипт установки зависимостей

--- src/install-dependences.sh
+++ src/install-dependences.sh
@@ -78,26 +78,5 @@ rm *gz
 mv eza /usr/bin
 
 ### Build Pop-Shell
-# dependencies
-mkdir -p /var/roothome/.npm
-mkdir -p /var/usrlocal
-rpm-ostree install \
-    nodejs \
-    npm
-npm install -g typescript
-# download
-mkdir -p /tmp/pop_shell
-cd /tmp/pop_shell
-git clone -b master_mantic https://github.com/pop-os/shell.git shell
-# setup
-cd shell
-DESTDIR=/tmp/compile_pop_shell
-mkdir -p $DESTDIR
-sed -i '1s/^/DESTDIR = \/tmp\/compile_pop_shell\n\n/' Makefile
-make depcheck && make compile && make install
-cd $DESTDIR
-cp -r usr/* /usr/
-# clean up
-rpm-ostree install \
-    nodejs \
-    npm
+cmod +x install-dependences/pop_shell.sh
+./install-dependences/pop_shell.sh
