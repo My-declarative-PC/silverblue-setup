8c82cb0ef9fad9381a6c07f43f39a39d27785406
ImperatorMarsa <tima.sitnikov@mail.ru>
2023-11-18 13:36:31 +0300

    Явно указал тему

--- src/set-theme.sh
+++ src/set-theme.sh
@@ -35,7 +35,7 @@ cp -r Catppuccin-Mocha-${THONE}-Cursors /usr/share/icons
 # dependencies
 rpm-ostree install glib2-devel
 # setup
-THEME_NAME="$(gsettings get org.gnome.desktop.interface gtk-theme | sed "s/'//g")"
+THEME_NAME=Catppuccin
 THEME_SRC_DIR="/usr/share/themes/$THEME_NAME/gnome-shell"
 glib-compile-resources --target="/usr/share/gnome-shell/gnome-shell-theme.gresource" --sourcedir="$THEME_SRC_DIR" "$THEME_SRC_DIR/gnome-shell-theme.gresource.xml"
 # cleanup
