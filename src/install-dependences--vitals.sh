aa077d881db402044bec6f3adbfd915fbfcd5b70
ImperatorMarsa <tima.sitnikov@mail.ru>
2024-01-30 22:01:42 +0300

    Удаил мкрипт установки Vitals

--- src/install-dependences--vitals.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-mkdir -p /tmp/vitals
-
-cd /tmp/vitals
-wget https://github.com/corecoding/Vitals/releases/latest/download/vitals.zip -O vitals.zip
-
-unzip vitals.zip -d /usr/share/gnome-shell/extensions/Vitals@CoreCoding.com
