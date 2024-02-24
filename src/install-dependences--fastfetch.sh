9201becb3cebb1904c20f643b279d2b9882963bc
ImperatorMarsa <tima.sitnikov@mail.ru>
2024-02-24 17:18:26 +0300

    Ispravil script ustanovki `fastfetch`
    tak kak afftor izmenil maimenovanie faylov

--- src/install-dependences--fastfetch.sh
+++ src/install-dependences--fastfetch.sh
@@ -5,7 +5,7 @@ mkdir -p /tmp/fastfetch
 cd /tmp/fastfetch
 wget https://api.github.com/repos/fastfetch-cli/fastfetch/tags
 VERSION=$(cat tags | grep name | grep -e '"[0-9]' | head -1 | sed 's/.*"\([0-9]\+\.[0-9]\+\.[0-9]\+\)".*/\1/g')
-wget https://github.com/fastfetch-cli/fastfetch/releases/download/${VERSION}/fastfetch-${VERSION}-Linux.tar.gz -O fastfetch.tar.gz
+wget https://github.com/fastfetch-cli/fastfetch/releases/download/${VERSION}/fastfetch-linux-amd64.tar.gz -O fastfetch.tar.gz
 tar -xf fastfetch.tar.gz
 
 rm *gz
