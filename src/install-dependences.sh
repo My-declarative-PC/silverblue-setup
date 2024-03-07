b60f8f2def30011cefe8b56734cbd85b3565ee25
ImperatorMarsa <tima.sitnikov@mail.ru>
2024-03-07 15:06:47 +0300

    добавил зависимости требуемые для `ueberzugpp`

--- src/install-dependences.sh
+++ src/install-dependences.sh
@@ -66,6 +66,14 @@ rpm-ostree install       \
 # yazi
 /tmp/docker_src/install-dependences--yazi.sh
 # ueberzugpp
+rpm-ostree install --apply-live \
+    opencv-imgcodecs \
+    opencv-core \
+    opencv-imgproc \
+    opencv-videoio \
+    libsixel \
+    tbb2020 \
+    vips
 sudo rpm -i https://download.opensuse.org/repositories/home:/justkidding/Fedora_39/x86_64/ueberzugpp-2.9.4-4.1.x86_64.rpm
 # font 'Timse New Roman'
 rpm-ostree install --apply-live cabextract
