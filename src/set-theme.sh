b1ea455e9339c56e0c856ded4c3bb4d890be21f0
ImperatorMarsa <tima.sitnikov@mail.ru>
2024-04-13 20:41:43 +0300

    Добавид установку темы для `qt5ct`

--- src/set-theme.sh
+++ src/set-theme.sh
@@ -13,6 +13,15 @@ unzip gtk_theme.zip
 mkdir -p /usr/share/themes
 mv Catppuccin-Mocha-Standard-${THONE}-Dark /usr/share/themes/Catppuccin
 
+### QT
+# download
+TMP_DIR=/tmp/qt_theme
+git clone https://github.com/catppuccin/qt5ct.git ${TMP_DIR}
+cd ${TMP_DIR}
+# setup
+mkdir -p /usr/share/qt5ct/colors
+mv themes/Cat*Mocha* /usr/share/qt5ct/colors/Catppuccin.conf
+
 ### Icons
 # download
 TMP_DIR=/tmp/papirus
