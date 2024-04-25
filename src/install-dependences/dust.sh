713264596dbd7fc5e9160a18337f17c900654a55
ImperatorMarsa <tima.sitnikov@mail.ru>
2024-04-25 21:27:28 +0300

    Исправил ачипятку

--- src/install-dependences/dust.sh
+++ src/install-dependences/dust.sh
@@ -6,7 +6,7 @@ mkdir -p $TMP_PATH
 cd $TMP_PATH
 URL=https://api.github.com/repos/bootandy/dust/tags
 VERSION=$(curl -fL $URL | jq 'max_by(.name).name' | sed 's/.*v\(.*\)".*/\1/g')
-URL=https://github.com/bootandy/dust/releases/download/${VERSION}/dust-${VERSION}-x86_64-unknown-linux-gnu.tar.gz
+URL=https://github.com/bootandy/dust/releases/download/v${VERSION}/dust-v${VERSION}-x86_64-unknown-linux-gnu.tar.gz
 echo "<<< ${URL} >>>"
 curl -fL $URL -o dust.tar.gz
 
