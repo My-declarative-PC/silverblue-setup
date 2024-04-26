d1bc56679a8964b4bfb8ca9220b5a2ca3c5a1a47
Timofey Sitnikov <t.sitnikov@infocom-s.ru>
2024-04-26 11:13:34 +0300

    Добавил установку `helix`

---
+++ src/install-dependences/helix.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+rpm-ostree install --apply-live helix
+
+TMP_PATH=/tmp/helix
+mkdir -p $TMP_PATH
+
+cd $TMP_PATH
+URL=https://api.github.com/repos/helix-editor/helix/tags
+VERSION=$(curl -fL $URL | jq -r '.[] | select(.name | test("^v") | not) | .name' | sort -V | tail -n 1)
+URL=https://github.com/helix-editor/helix/releases/download/${VERSION}/helix-${VERSION}-x86_64-linux.tar.xz
+echo "<<< ${URL} >>>"
+curl -fL $URL -o helix.tar.gz
+
+tar -xf helix.tar.gz
+rm *gz
+cd he*
+
+mv -f ./hx /usr/bin/hx
+mv -f ./runtime /usr/lib64/helix/runtime
+
+cd /
+rm -rf $TMP_PATH
