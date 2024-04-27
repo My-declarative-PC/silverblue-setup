32620d4223af0314718925db61665315b1e08d7f
ImperatorMarsa <tima.sitnikov@mail.ru>
2024-04-27 21:29:54 +0300

    Добавил `starship`

--- src/install-dependences/starship.sh
+++ src/install-dependences/starship.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-PACKAGE_NAME=lazygit
+PACKAGE_NAME=starship
 TMP_PATH=/tmp/${PACKAGE_NAME}
 
 mkdir -p $TMP_PATH
 
 cd $TMP_PATH
-URL=https://api.github.com/repos/jesseduffield/${PACKAGE_NAME}/tags
+URL=https://api.github.com/repos/starship/${PACKAGE_NAME}/tags
 VERSION=$(curl -fL $URL | jq 'max_by(.name).name' | sed 's/.*v\(.*\)".*/\1/g')
-URL=https://github.com/jesseduffield/${PACKAGE_NAME}/releases/download/v${VERSION}/${PACKAGE_NAME}_${VERSION}_Linux_x86_64.tar.gz
+URL=https://github.com/starship/starship/releases/download/v${VERSION}/${PACKAGE_NAME}-x86_64-unknown-linux-gnu.tar.gz
 echo "<<< ${URL} >>>"
 curl -fL $URL -o ${PACKAGE_NAME}.tar.gz
 
