f8aa2b5d3d25a0ca10fdb208d8369adac949301f
Timofey Sitnikov <t.sitnikov@infocom-s.ru>
2024-04-27 12:13:43 +0300

    Изменил способ установки шрихта

--- src/install-dependences/font-FiraCode.sh
+++ src/install-dependences/font-FiraCode.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
 ### FiraCode
-git clone https://github.com/ryanoasis/nerd-fonts.git /tmp/fonts
-cd /tmp/fonts
+mkdir -p /tmp/fonts_FiraCode
+cd /tmp/fonts_FiraCode
 
-chmod +x ./install.sh
-./install.sh --clean -install-to-system-path --use-proportional-glyphs --ttf FiraCode
+curl -Lf -o FiraCode.tar.xz https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.tar.xz
+tar -xf FiraCode.tar.xz
+
+mkdir -p /usr/share/fonts/FiraCode
+mv *.ttf /usr/share/fonts/FiraCode
 
 cd
-rm -rf /tmp/fonts
+rm -rf /tmp/fonts_FiraCode
