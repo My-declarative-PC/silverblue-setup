f7d3536599e023bcbd39d4b73555dea17e464b72
Timofey Sitnikov <t.sitnikov@infocom-s.ru>
2024-05-21 17:16:20 +0300

    Добавил пакеты "сетевого манагера"

---
+++ src/install-dependences/NetworkManager-from-repository.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+### nm tools
+rpm-ostree install -y    \
+NetworkManager           \
+NetworkManager-bluetooth \
+NetworkManager-ssh       \
+NetworkManager-tui       \
+NetworkManager-wifi      \
