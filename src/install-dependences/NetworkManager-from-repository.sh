8e17c4c15070402d60d60a731bfb73f06bfa5543
ImperatorMarsa <tima.sitnikov@mail.ru>
2024-06-11 20:53:46 +0300

    Удалил лишние пакеты

--- src/install-dependences/NetworkManager-from-repository.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-### nm tools
-rpm-ostree install -y    \
-NetworkManager           \
-NetworkManager-bluetooth \
-NetworkManager-ssh       \
-NetworkManager-tui       \
-NetworkManager-wifi      \
